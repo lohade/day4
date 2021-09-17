30564c7c6f2e783d466a1ed6e9b848a9ce7e79c5
lohade <abhisheklohade8584@gmail.com>
2021-09-17 15:04:22 +0530

    after first dice

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -6,6 +6,10 @@ public class SnakeLadder {
         int playerposition = 0;
         System.out.println("The position of player is : " + playerposition);
 
+        int diceroll = (int) (Math.random() * 6) + 1;
+        playerposition += diceroll;
+        System.out.println("After the roll dice....The position of player is : " + playerposition);
+
 
     }
 }
