71c6a96755cd86e4b7525c8ec6a9427fa88d4b42
lohade <abhisheklohade8584@gmail.com>
2021-09-17 15:41:10 +0530

    player position after dice roll

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,13 +1,27 @@
+import java.util.Random;
+
 public class SnakeLadder {
+    public static final int No_Play = 0;
+    public static final int Ladder = 1;
+    public static final int Snake = 2;
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Welcome to snake and ladder:");
         int playerposition = 0;
-        System.out.println("The position of player is : " + playerposition);
-
-        int diceroll = (int) (Math.random() * 6) + 1;
-        playerposition += diceroll;
+        System.out.println("The start position of player is : " + playerposition);
+        Random ran=new Random();
+        int diceroll = ran.nextInt(6) + 1;
+        int check_play = ran.nextInt(3);
+        switch (check_play) {
+            case No_Play:
+                break;
+            case Ladder:
+                playerposition += diceroll;
+                break;
+            case Snake:
+                playerposition -= diceroll;
+                break;
+        }
         System.out.println("After the roll dice....The position of player is : " + playerposition);
 
 
