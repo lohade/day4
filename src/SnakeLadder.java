2acb8c8721f44e849403c0d030590b4ea3ffb736
lohade <abhisheklohade8584@gmail.com>
2021-09-17 14:47:25 +0530

    player 1 position

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -2,6 +2,10 @@ public class SnakeLadder {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Welcome to snake and ladder:");
+        int playerposition = 0;
+        System.out.println("The position of player is : " + playerposition);
+
 
     }
 }
